4f2652a01db5fec500235317adb3e01e331520c2
Justin Hayes <justin@justinhayes.me>
2023-02-12 06:12:58 -0500

    Updating CORS origin.

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: 'https://ellada-ui.fly.dev',
+    origin: 'https://justinhayes.me',
   });
   await app.listen(process.env.PORT ? parseInt(process.env.PORT) : 3000);
 }
