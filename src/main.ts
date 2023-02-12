d00ecfd037aada3d1473d20c74178a77ed025855
Justin Hayes <justin@justinhayes.me>
2023-02-11 19:13:18 -0500

    Configuring CORS.

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,9 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: 'https://ellada-ui.fly.dev',
+  });
   await app.listen(process.env.PORT ? parseInt(process.env.PORT) : 3000);
 }
 bootstrap();
